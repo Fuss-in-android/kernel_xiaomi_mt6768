645ff64fd2a96b3d6cde5125bf00b2c7f348b676
Matsvei Niaverau <matvej2002@gmail.com>
2022-07-09 13:26:17 +0300

    Merge tag '4.14.286' of https://android.googlesource.com/kernel/common into twelve

--- drivers/of/fdt.c
+++ drivers/of/fdt.c
@@ -1187,8 +1187,6 @@ int __init early_init_dt_scan_chosen(unsigned long node, const char *uname,
 		/* update CRC check value */
 		of_fdt_crc32 = crc32_be(~0, initial_boot_params,
 				fdt_totalsize(initial_boot_params));
-
-
 	}
 
 	/* break now */
@@ -1302,6 +1300,8 @@ bool __init early_init_dt_verify(void *params)
 
 	/* Setup flat device-tree pointer */
 	initial_boot_params = params;
+	of_fdt_crc32 = crc32_be(~0, initial_boot_params,
+				fdt_totalsize(initial_boot_params));
 	return true;
 }
 
@@ -1327,8 +1327,6 @@ bool __init early_init_dt_scan(void *params)
 		return false;
 
 	early_init_dt_scan_nodes();
-	of_fdt_crc32 = crc32_be(~0, initial_boot_params,
-				fdt_totalsize(initial_boot_params));
 	return true;
 }
 
